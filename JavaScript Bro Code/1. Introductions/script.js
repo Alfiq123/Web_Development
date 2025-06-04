04f32ef18c539f28bf9252d65fad55cc7cf63bdd
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-06-04 21:14:57 +0700

    Initial commit

---
+++ JavaScript Bro Code/1. Introductions/script.js
@@ -0,0 +1,12 @@
+// Basic Console.
+console.log("Hello World");
+console.log("JavaScript is the best");
+console.log("World Domination");
+
+// Alert Windows.
+window.alert("WARNING: System is under attack");
+window.alert("WARNING: System is under maintenance");
+
+// Get Element by ID.
+document.getElementById("I1").textContent = "Hello World";
+document.getElementById("I2").textContent = "The Ultimate JavaScript Course";
