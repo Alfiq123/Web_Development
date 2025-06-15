8ee150b4140a9852d9c7207ae935d9d089855265
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-06-15 23:07:19 +0700

    💡 Training 1.0.3 - Adding Something

---
+++ Dea Afrizal - JavaScript/1. main.js
@@ -0,0 +1,13 @@
+// Showing Alert? //
+// alert("JavaScript is King");
+
+// Log //
+// console.log("JavaScript is King");
+
+// Input //
+// prompt("Enter Something you like");
+
+// Variable //
+// let something;
+// something = prompt("Enter Something you like");
+// alert(`You like ${something}`)
