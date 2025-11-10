33761ec9f932416de19fdce217ee3d825d805e51
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-11-11 00:06:55 +0700

    🛫 1.0.5 Maint
    - Adding many things.

---
+++ Web_Development_YouTube/Agung Hafsah/comparison.js
@@ -0,0 +1,29 @@
+let stock = 100;
+
+if (stock <= 0) {
+    console.log("Produk Sudah Habis!");
+} else {
+    console.log("Produk Masih Tersedia!");
+}
+
+let x = 12 < 24;
+
+if (x) {
+    console.log("Benar Cuy!");
+} else {
+    console.log("Salah Cuy!");
+}
+
+// Challenge I
+
+console.log(10 > 5);
+console.log(8 < 4);
+console.log(-1 >= 2);
+console.log(0.5 <= 0);
+
+// Challenge II
+
+console.log("abc" === "abc");
+console.log(false !== true);
+console.log("cab" === "cba");
+console.log(false === false);
