b68a0c3dd8bda800f41a4b259a363c814195f2f6
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-11-14 21:48:30 +0700

    🛫 1.0.7 Maint
    - Add: `9._undefinednull.js`

---
+++ Web_Development_YouTube/Agung Hafsah/9._undefinednull.js
@@ -0,0 +1,19 @@
+let undef = undefined;
+let nulls = null;
+
+let pet = null;
+
+console.log(typeof(undef));
+console.log(typeof(nulls));
+
+console.log(undef == nulls);
+console.log(undef === nulls);
+
+// Challenge I
+
+let shoe = "Nike";
+let bike = null;
+let motorcycle = "Honda";
+let car = null;
+let boat = null;
+let airplane = null;
